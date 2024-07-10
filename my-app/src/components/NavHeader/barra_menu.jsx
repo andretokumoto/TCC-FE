eccca14e11959143caff30bb768f4c6cff003dcd
andretokumoto <andretokumoto@gmail.com>
2024-07-10 16:36:49 -0300

    criação de novo arquivo para barra de menu

---
+++ my-app/src/components/NavHeader/barra_menu.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import './navheader.css'; 
+
+//const ItensNovo = []
+
+const ItensInserir = ['Imagem','Tabela']
+
+
+
+const BarraDeMenu = () => {
+
+
+
+
+}
+
+export default BarraDeMenu;
