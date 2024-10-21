b287e1274b029113dcd71f37871ebb5390125ab2
andretokumoto <andretokumoto@gmail.com>
2024-10-21 15:41:09 -0300

    renomendo arquivo

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter/*, Route, Routes, Navigate*/ } from 'react-router-dom';
 import { React/*, useContext */} from 'react';
 import {  createTheme,ThemeProvider } from '@mui/material';
-import NavHeader from './components/NavHeader/barra_menu.jsx'
+import NavHeader from './components/NavHeader/navheader.jsx'
 
 
 //import GlobalStyles from './styles/GlobalStyles';
