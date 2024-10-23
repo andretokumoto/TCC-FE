1abb17dee99f27d4ed28cc9d3ccb6ecc8e31f241
andretokumoto <andretokumoto@gmail.com>
2024-10-23 14:31:05 -0300

    mudança no alinhamento do titulo

--- my-app/src/components/NavHeader/navheader.jsx
+++ my-app/src/components/NavHeader/navheader.jsx
@@ -37,7 +37,7 @@ const NavHeader = () => {
             <Toolbar className='toolbar'>
     
                 <div className='button_left'>
-                    <SimpleButton nameButton='Novo Projeto'/>
+                    <SimpleButton nameButton='Novo'/>
                     <DropdownButton options={ItensInserir} nameButton='Inserir' />
                 </div>
 
