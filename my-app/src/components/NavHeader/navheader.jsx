1c81ef542ce3c039d3704f0681359d3c044f4916
andretokumoto <andretokumoto@gmail.com>
2025-01-16 16:41:44 -0300

    colocação da lista suspensa no arquivo navheader

--- my-app/src/components/NavHeader/navheader.jsx
+++ my-app/src/components/NavHeader/navheader.jsx
@@ -1,73 +1,122 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PropTypes from 'prop-types';
 import './navheader.css';
-import DropdownButton from './listaSuspensa.jsx' 
-//import { htmlToText } from 'html-to-text' //para alterar o texto de entrada para formato latex
 
+const ItensInserir = ['Imagem', 'Tabela'];
+const ItensBaixar = ['PDF', 'LaTeX'];
 
-const ItensInserir = ['Imagem','Tabela']
-const ItensBaixar = ['PDF','LaTeX']
+// Componente DropdownButton (Lista Suspensa)
+const DropdownButton = ({ options, nameButton, onSelect }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-//botões simples, sem listas suspensas
-const SimpleButton = (props) =>{
-    const nameButton  = props.nameButton
-    return(
-        <Button variant="text" className='butom_simple'>{nameButton}</Button>
-    )
-}
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-const NavHeader = () => {
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const [titulo, setTitulo] = useState('Título do Projeto')//varivel que armazena titulo
-    
-    // Função para atualizar o estado com o valor digitado
-    const handleTituloChange = (event) => {
-        setTitulo(event.target.value); 
-    };
+  const handleOptionSelect = (option) => {
+    handleClose();
+    if (onSelect) {
+      onSelect(option); 
+    }
+  };
 
-    // Função chamada ao clicar em "Compilar"
- /*   const handleCompile = () => {
-        handleSend({ titulo });
-    };*/
+  return (
+    <div>
+      <Button
+        aria-controls={open ? 'dropdown-menu' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+        variant="text"
+        className="butom_simple"
+      >
+        {nameButton}
+      </Button>
+      <Menu
+        id="dropdown-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'dropdown-button',
+        }}
+      >
+        {options.map((option, index) => (
+          <MenuItem key={index} onClick={() => handleOptionSelect(option)}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+};
 
-    /*
-        TextField cria o elemento de entrada do titulo
-    */
-    return (
-        <AppBar className='header'>
-            
-            <Toolbar className='toolbar'>
-    
-                <div className='button_left'>
-                    <SimpleButton nameButton='Novo'/>
-                    <DropdownButton options={ItensInserir} nameButton='Inserir' />
-                </div>
+DropdownButton.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  nameButton: PropTypes.string.isRequired,
+  onSelect: PropTypes.func,
+};
 
-                <div className='text_field_title'>
-                    <TextField
-                        
-                        value={titulo}
-                        onChange={handleTituloChange}
-                        //puxa a estilização do css para o valor de entrada
-                        inputProps={{
-                            className: "input_title" }}
-                    />
-                </div>
-                <div className='button_right'>
-                     <SimpleButton nameButton='Compilar' /*onClick={handleCompile}*//>
-                     <DropdownButton options={ItensBaixar} nameButton='Baixar' />
-                </div>
+// Botão simples
+const SimpleButton = ({ nameButton, onClick }) => (
+  <Button variant="text" className="butom_simple" onClick={onClick}>
+    {nameButton}
+  </Button>
+);
 
+const NavHeader = ({ onCompile, onDownload }) => {
+  const [titulo, setTitulo] = useState('Título do Projeto');
 
+  const handleTituloChange = (event) => {
+    setTitulo(event.target.value);
+  };
 
-            </Toolbar>
-        
-        </AppBar>
-      )
+  return (
+    <AppBar className="header">
+      <Toolbar className="toolbar">
+        <div className="button_left">
+          <SimpleButton nameButton="Novo" />
+          <DropdownButton
+            options={ItensInserir}
+            nameButton="Inserir"
+            onSelect={(option) => console.log(`Inserir: ${option}`)}
+          />
+        </div>
 
-}
+        <div className="text_field_title">
+          <TextField
+            value={titulo}
+            onChange={handleTituloChange}
+            inputProps={{ className: 'input_title' }}
+          />
+        </div>
+
+        <div className="button_right">
+          <SimpleButton nameButton="Compilar" onClick={() => onCompile(titulo)} />
+          <DropdownButton
+            options={ItensBaixar}
+            nameButton="Baixar"
+            onSelect={(option) => onDownload(option)}
+          />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+NavHeader.propTypes = {
+  onCompile: PropTypes.func.isRequired,
+  onDownload: PropTypes.func.isRequired,
+};
 
 export default NavHeader;
